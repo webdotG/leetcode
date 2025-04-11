75d18285564b1a9341373eeb32a5febb06ea729e
webdotG <webdotg@gmail.com>
2025-04-12 01:27:56 +0300

    add

--- base_ts/base.ts
+++ base_ts/base.ts
@@ -1,4 +1,50 @@
-type ToBeOrNotToBe = {
+//
+// BASE ========BASE>>===========BASE??================BASE>>================BASE??===============BASE>>=======================??===>>===
+//
+ //169. Majority Element-----------------------------------------------------------------------
+ //надо найти мажоритарный элемент 
+ // — число, которое встречается больше ⌊n / 2⌋ раз
+ //Алгоритм Бойера-Мура! 
+//  Используем переменную candidate (кандидат) и count (счётчик):
+//  Если count = 0, выбираем текущий элемент как candidate.
+//  Если текущий элемент равен candidate, увеличиваем count.
+//  Если не равен, уменьшаем count
+//  В конце candidate будет мажоритарным элементом.
+// Время: O(n) Память: O(1)
+ function majorityElement(nums: number[]): number {
+    let count = 1
+    let result = nums[0]
+  
+    for (let i = 1; i < nums.length; i++) {
+      if (count === 0) {
+        result = nums[i]
+        count = 1
+      }
+  
+      if (result === nums[i]) count++
+      else count--
+    }
+  
+    return result
+  };
+// Хэш-таблица Время: O(n), память: O(n)
+  function majorityElementHash(nums: number[]): number {
+    const map = new Map<number, number>();
+    const threshold = Math.floor(nums.length / 2);
+    for (const num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+        if (map.get(num)! > threshold) return num;
+    }
+    return 0; // Никогда не дойдёт сюда по условию
+}
+// Сортировка Время: O(n log n), память: O(1) (если in-place).
+function majorityElementSort(nums: number[]): number {
+    nums.sort((a, b) => a - b);
+    return nums[Math.floor(nums.length / 2)];
+}
+console.log('МАЖОРИТАРНЫЙ ЭЛЕМЕНТ встречается больше ⌊n / 2⌋ раз [2,2,1,1,1,2,2,1,1,1,1,1,3,3,3,3,4,4,4,4]', majorityElement([2,2,1,1,1,2,2,1,1,1,1,1,3,3,3,3,4,4,4,4]))
+
+ type ToBeOrNotToBe = {
     toBe: (val: any) => boolean;
     notToBe: (val: any) => boolean;
 };
--- dist/base_ts/base.js
+++ dist/base_ts/base.js
@@ -1,4 +1,50 @@
 "use strict";
+//
+// BASE ========BASE>>===========BASE??================BASE>>================BASE??===============BASE>>=======================??===>>===
+//
+//169. Majority Element-----------------------------------------------------------------------
+//надо найти мажоритарный элемент 
+// — число, которое встречается больше ⌊n / 2⌋ раз
+//Алгоритм Бойера-Мура! 
+//  Используем переменную candidate (кандидат) и count (счётчик):
+//  Если count = 0, выбираем текущий элемент как candidate.
+//  Если текущий элемент равен candidate, увеличиваем count.
+//  Если не равен, уменьшаем count
+//  В конце candidate будет мажоритарным элементом.
+// Время: O(n) Память: O(1)
+function majorityElement(nums) {
+    let count = 1;
+    let result = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (count === 0) {
+            result = nums[i];
+            count = 1;
+        }
+        if (result === nums[i])
+            count++;
+        else
+            count--;
+    }
+    return result;
+}
+;
+// Хэш-таблица Время: O(n), память: O(n)
+function majorityElementHash(nums) {
+    const map = new Map();
+    const threshold = Math.floor(nums.length / 2);
+    for (const num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+        if (map.get(num) > threshold)
+            return num;
+    }
+    return 0; // Никогда не дойдёт сюда по условию
+}
+// Сортировка Время: O(n log n), память: O(1) (если in-place).
+function majorityElementSort(nums) {
+    nums.sort((a, b) => a - b);
+    return nums[Math.floor(nums.length / 2)];
+}
+console.log('МАЖОРИТАРНЫЙ ЭЛЕМЕНТ встречается больше ⌊n / 2⌋ раз [2,2,1,1,1,2,2,1,1,1,1,1,3,3,3,3,4,4,4,4]', majorityElement([2, 2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 3, 3, 3, 3, 4, 4, 4, 4]));
 function expect(val) {
     let original_value = val;
     return {
